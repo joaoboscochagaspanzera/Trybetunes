3317d92ecac4689b6ac6f39ce472760963f41bfc
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-03-15 16:25:39 -0300

    Requisito 5, lint verificado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import Header from '../components/Header';
 
+const MINARTISTNAME = 2;
+
 class search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
   render() {
+    const { artistName } = this.state;
+    const searchButton = artistName.length < MINARTISTNAME;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>dentro da search</p>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="artistName"
+          value={ artistName }
+          onChange={ this.onInputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          disabled={ searchButton }
+          onClick={ this.saveName }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
